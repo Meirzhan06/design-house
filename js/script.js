b9a0638fb8ce3d2ed0b59f70165372039186a13d
Anonim <103999938+Meirzhan06@users.noreply.github.com>
2023-01-23 22:33:42 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -15,5 +15,20 @@ $(document).ready(function(){
         dots:true,
         slidesToShow:3,
         slidesToScroll:2,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll:1
+              }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
     });
 });
