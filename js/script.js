c00ac761df650efb39990bae3d43a4e17776ec50
Anonim <103999938+Meirzhan06@users.noreply.github.com>
2023-01-25 23:47:12 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -10,6 +10,319 @@ $(document).ready(function() {
 
 
 
+
+
+
+
+
+(function () {
+    var square = document.querySelector('.nav2');
+  
+    var observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (typeof getCurrentAnimationPreference === 'function' && !getCurrentAnimationPreference()) {
+          return;
+        }
+  
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animation');
+        }
+      });
+    });
+  
+    observer.observe(square);
+  })();
+
+(function () {
+    var square = document.querySelector('.subscribe');
+  
+    var observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (typeof getCurrentAnimationPreference === 'function' && !getCurrentAnimationPreference()) {
+          return;
+        }
+  
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animation');
+        }
+      });
+    });
+  
+    observer.observe(square);
+  })();
+(function () {
+    var square = document.querySelector('.content-square');
+  
+    var observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (typeof getCurrentAnimationPreference === 'function' && !getCurrentAnimationPreference()) {
+          return;
+        }
+  
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animation');
+        }
+      });
+    });
+  
+    observer.observe(square);
+  })();
+(function () {
+    var square = document.querySelector('.logo');
+  
+    var observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (typeof getCurrentAnimationPreference === 'function' && !getCurrentAnimationPreference()) {
+          return;
+        }
+  
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animation');
+        }
+      });
+    });
+  
+    observer.observe(square);
+  })();
+(function () {
+    var square = document.querySelector('.section2-1');
+  
+    var observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (typeof getCurrentAnimationPreference === 'function' && !getCurrentAnimationPreference()) {
+          return;
+        }
+  
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animation');
+        }
+      });
+    });
+  
+    observer.observe(square);
+  })();
+(function () {
+    var square = document.querySelector('.a');
+  
+    var observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (typeof getCurrentAnimationPreference === 'function' && !getCurrentAnimationPreference()) {
+          return;
+        }
+  
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animation');
+        }
+      });
+    });
+  
+    observer.observe(square);
+  })();
+(function () {
+    var square = document.querySelector('.b');
+  
+    var observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (typeof getCurrentAnimationPreference === 'function' && !getCurrentAnimationPreference()) {
+          return;
+        }
+  
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animation');
+        }
+      });
+    });
+  
+    observer.observe(square);
+  })();
+(function () {
+    var square = document.querySelector('.c');
+  
+    var observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (typeof getCurrentAnimationPreference === 'function' && !getCurrentAnimationPreference()) {
+          return;
+        }
+  
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animation');
+        }
+      });
+    });
+  
+    observer.observe(square);
+  })();
+(function () {
+    var square = document.querySelector('.d');
+  
+    var observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (typeof getCurrentAnimationPreference === 'function' && !getCurrentAnimationPreference()) {
+          return;
+        }
+  
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animation');
+        }
+      });
+    });
+  
+    observer.observe(square);
+  })();
+(function () {
+    var square = document.querySelector('.e');
+  
+    var observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (typeof getCurrentAnimationPreference === 'function' && !getCurrentAnimationPreference()) {
+          return;
+        }
+  
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animation');
+        }
+      });
+    });
+  
+    observer.observe(square);
+  })();
+(function () {
+    var square = document.querySelector('.section3');
+  
+    var observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (typeof getCurrentAnimationPreference === 'function' && !getCurrentAnimationPreference()) {
+          return;
+        }
+  
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animation');
+        }
+      });
+    });
+  
+    observer.observe(square);
+  })();
+
+(function () {
+    var square = document.querySelector('.section4');
+  
+    var observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (typeof getCurrentAnimationPreference === 'function' && !getCurrentAnimationPreference()) {
+          return;
+        }
+  
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animation');
+        }
+      });
+    });
+  
+    observer.observe(square);
+  })();
+(function () {
+    var square = document.querySelector('.f');
+  
+    var observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (typeof getCurrentAnimationPreference === 'function' && !getCurrentAnimationPreference()) {
+          return;
+        }
+  
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animation');
+        }
+      });
+    });
+  
+    observer.observe(square);
+  })();
+(function () {
+    var square = document.querySelector('.g');
+  
+    var observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (typeof getCurrentAnimationPreference === 'function' && !getCurrentAnimationPreference()) {
+          return;
+        }
+  
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animation');
+        }
+      });
+    });
+  
+    observer.observe(square);
+  })();
+(function () {
+    var square = document.querySelector('.h');
+  
+    var observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (typeof getCurrentAnimationPreference === 'function' && !getCurrentAnimationPreference()) {
+          return;
+        }
+  
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animation');
+        }
+      });
+    });
+  
+    observer.observe(square);
+  })();
+(function () {
+    var square = document.querySelector('.i');
+  
+    var observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (typeof getCurrentAnimationPreference === 'function' && !getCurrentAnimationPreference()) {
+          return;
+        }
+  
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animation');
+        }
+      });
+    });
+  
+    observer.observe(square);
+  })();
+(function () {
+    var square = document.querySelector('.j');
+  
+    var observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (typeof getCurrentAnimationPreference === 'function' && !getCurrentAnimationPreference()) {
+          return;
+        }
+  
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animation');
+        }
+      });
+    });
+  
+    observer.observe(square);
+  })();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function(){
     $('.slider').slick({
         arrows:false,
